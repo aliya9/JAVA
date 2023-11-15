f3b529e7921f1763822c43bcfb5146493fe1760d
Farisa Aliya <farisaliya2004@gmail.com>
2023-11-16 00:34:35 +1100

    dispBoard()

--- TicTacToe/LaunchGame.java
+++ TicTacToe/LaunchGame.java
@@ -1,5 +1,62 @@
 package TicTacToe;
 
+class TicTacToe
+{
+    char[][] board; 
+
+    public TicTacToe()
+    {
+        board = new char[3][3];
+        initBoard();
+    }
+
+    void initBoard()
+    {
+        for(int i=0; i<board.length; i++)
+        {
+            for(int j=0; j<board[i].length; j++)
+            {
+                board[i][j]= ' ';
+            }
+        }
+    }
+
+    void dispBoard()
+    {
+        System.out.println("-------------");
+        for(int i=0; i<board.length; i++)
+        {
+            System.out.print("| ");
+            for(int j=0; j<board[i].length; j++)
+            {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+        System.out.println("-------------");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class LaunchGame {
+
+    public static void main(String[] args) {
+        TicTacToe t = new TicTacToe();
+        t.dispBoard();
+
+    }
     
 }
